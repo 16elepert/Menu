596ceb3c5d7fa83db98043b98e7fabcd26207cb4
Emily2 <Emily2@Arnauds-MacBook-Pro.local>
2015-04-21 08:47:07 -0700

    First Screen and Rating Screen (not completed yet)

--- CastiMenu/AppDelegate.swift
+++ CastiMenu/AppDelegate.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+import Parse
+import Bolts
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -16,6 +18,15 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
+        // [Optional] Power your app with Local Datastore. For more info, go to
+        // https://parse.com/docs/ios_guide#localdatastore/iOS
+        Parse.enableLocalDatastore()
+        
+        // Initialize Parse.
+        Parse.setApplicationId("Ub5WvG9piPrerSTkD0DPH5iRqkDmVkhkge3hMyKZ", clientKey: "SdHx8Ij6b3ElF4GRgk6DkROfyCrMNkB53WJnOU2h")
+        
+        // [Optional] Track statistics around application opens.
+        PFAnalytics.trackAppOpenedWithLaunchOptions(launchOptions)
         return true
     }
 
--- CastiMenu/SecondViewController.swift
+++ CastiMenu/SecondViewController.swift
@@ -8,18 +8,61 @@
 
 import UIKit
 
-class SecondViewController: UIViewController {
+class SecondViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
 
+    @IBOutlet weak var entreePicker: UIPickerView!
+    
+    @IBOutlet weak var entreePickerLabel: UILabel!
+    
+    let pickerData = ["   No Rating", "   More often", "   Less often", "   It's fine"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        entreePicker.dataSource = self
+        entreePicker.delegate = self
+    }
+    
+    // code below is from: http://makeapppie.com/2014/10/21/swift-swift-formatting-a-uipickerview/
+    func numberOfComponentsInPickerView(pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return pickerData.count
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func pickerView(pickerView:UIPickerView, titleForRow row: Int, forComponent component: Int) -> String! {
+        return pickerData[row]
+    }
+    
+    func pickerView(pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        //entreePickerLabel.text = pickerData[row]
     }
 
-
+    func pickerView(pickerView: UIPickerView, viewForRow row: Int, forComponent component: Int, reusingView view: UIView!) -> UIView {
+        var pickerLabel = view as UILabel!
+        if view == nil {  //if no label there yet
+            pickerLabel = UILabel()
+            
+            //color  and center the label's background
+            pickerLabel.textAlignment = .Left
+            
+        }
+        let titleData = pickerData[row]
+        let myTitle = NSAttributedString(string: titleData, attributes: [NSFontAttributeName:UIFont(name: "Helvetica", size: 15.0)!,NSForegroundColorAttributeName:UIColor.blackColor()])
+        pickerLabel!.attributedText = myTitle
+        
+        return pickerLabel
+        
+    }
+    
+    func pickerView(pickerView: UIPickerView, rowHeightForComponent component: Int) -> CGFloat {
+        return 15.0
+    }
+    
+    func pickerView(pickerView: UIPickerView, widthForComponent component: Int) -> CGFloat {
+        return 125
+    }
 }
 

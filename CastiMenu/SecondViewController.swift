cf10033e578a563c9b4431275eb26c89436c1342
Emily2 <Emily2@Arnauds-MacBook-Pro.local>
2015-04-29 13:29:44 -0700

    Fixing Macho-Error after this

--- CastiMenu/SecondViewController.swift
+++ CastiMenu/SecondViewController.swift
@@ -8,60 +8,32 @@
 
 import UIKit
 
-class SecondViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
+class SecondViewController: UIViewController {
 
-    @IBOutlet weak var entreePicker: UIPickerView!
-    @IBOutlet weak var entreePickerLabel: UILabel!
-    
-    let pickerData = ["   No Rating", "   More often", "   Less often", "   It's fine"]
+    @IBOutlet weak var entreeLabel: UILabel!
+    @IBOutlet weak var entreeSlider: UISlider!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        entreePicker.dataSource = self
-        entreePicker.delegate = self
+        
     }
     
     // code below is from: http://makeapppie.com/2014/10/21/swift-swift-formatting-a-uipickerview/
-    func numberOfComponentsInPickerView(pickerView: UIPickerView) -> Int {
-        return 1
-    }
     
-    func pickerView(pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
-        return pickerData.count
-    }
-
-    func pickerView(pickerView:UIPickerView, titleForRow row: Int, forComponent component: Int) -> String! {
-        return pickerData[row]
-    }
-    
-    func pickerView(pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
-        //entreePickerLabel.text = pickerData[row]
-    }
-
-    func pickerView(pickerView: UIPickerView, viewForRow row: Int, forComponent component: Int, reusingView view: UIView!) -> UIView {
-        var pickerLabel = view as! UILabel!
-        if view == nil {  //if no label there yet
-            pickerLabel = UILabel()
-            
-            //color  and center the label's background
-            pickerLabel.textAlignment = .Left
-            
+    @IBAction func entreeSlider(sender: AnyObject) {
+        if 0 <= entreeSlider.value && entreeSlider.value < 0.5 {
+            entreeSlider.value = 0
+            entreeLabel.text = "N/A"
+        } else if 0.5 <= entreeSlider.value && entreeSlider.value < 1.5 {
+            entreeSlider.value = 1
+            entreeLabel.text = "Less"
+        } else if 1.5 <= entreeSlider.value && entreeSlider.value < 2.5 {
+            entreeSlider.value = 2
+            entreeLabel.text = "Neutral"
+        } else if 2.5 <= entreeSlider.value && entreeSlider.value < 3 {
+            entreeSlider.value = 3
+            entreeLabel.text = "More"
         }
-        let titleData = pickerData[row]
-        let myTitle = NSAttributedString(string: titleData, attributes: [NSFontAttributeName:UIFont(name: "Helvetica", size: 15.0)!,NSForegroundColorAttributeName:UIColor.blackColor()])
-        pickerLabel!.attributedText = myTitle
-        
-        return pickerLabel
-        
-    }
-    
-    func pickerView(pickerView: UIPickerView, rowHeightForComponent component: Int) -> CGFloat {
-        return 15.0
-    }
-    
-    func pickerView(pickerView: UIPickerView, widthForComponent component: Int) -> CGFloat {
-        return 125
-    }
 }
-
+}
